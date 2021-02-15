6fa00bb6fed100d11e27d2e35f747f867bc13e01
maliat-hossain <32250724+maliat-hossain@users.noreply.github.com>
2021-02-14 21:44:00 -0500

    Add files via upload

---
+++ SQL script For Assignment 2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `assignment_2_607`.`movie_rating_6` (
+  `Name` VARCHAR(45) NOT NULL,
+  `Movie 1 Promising Young woman` INT NULL,
+  `Movie 2 Little Things` INT NULL,
+  `Movie 3Wonder woman 1984` INT NULL,
+  `Movie 4 Soul` INT NULL,
+  `Movie 5 Bliss` INT NULL,
+  `Movie 6 Tenet` INT NULL,
+  PRIMARY KEY (`Name`));
+  
+  INSERT INTO `assignment_2_607`.`movie_rating_5`(`Name`,`Movie 1 Promising Young woman
+  `,`Movie 2 Little Things`,`Movie 3Wonder woman 1984`,`Movie 4 Soul`,`Movie 5 Bliss
+  `,`Movie 6 Tenet` INT NULL)VALUES('Fahi','4','3','4','5','3','4');
